fb73cbb0ce4dfd58b2c64787491a8f1335b259a3
elis0m <sominzzang615@gmail.com>
2020-05-07 23:27:29 +0900

    added skills' icon

--- js/main.js
+++ js/main.js
@@ -124,6 +124,13 @@ jQuery(document).ready(function( $ ) {
     allowPageScroll:"vertical"
   });
 
+  // Services section
+  $('#services').waypoint(function() {
+    $('.progress .progress-bar').each(function() {
+      $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    });
+  }, { offset: '80%'} );
+
   // Skills section
   $('#skills').waypoint(function() {
     $('.progress .progress-bar').each(function() {
