a7d4b063892cc7723efcaea92819cc59566248cb
elis0m <sominzzang615@gmail.com>
2020-12-01 22:43:32 +0900

    스크롤 이동에 따른 상단 메뉴 active 적용 및 css, image 추가

--- js/main.js
+++ js/main.js
@@ -31,6 +31,29 @@ jQuery(document).ready(function( $ ) {
     */
   });
 
+  // Navigation active state on scroll
+  var nav_sections = $('section');
+  var main_nav = $('.nav-menu, .mobile-nav');
+
+  $(window).on('scroll', function() {
+    var cur_pos = $(this).scrollTop() + 200;
+
+    nav_sections.each(function() {
+      var top = $(this).offset().top,
+        bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        if (cur_pos <= bottom) {
+          main_nav.find('li').removeClass('menu-active');
+        }
+        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('menu-active');
+      }
+      if (cur_pos < 300) {
+        $(".nav-menu ul:first li:first").addClass('menu-active');
+      }
+    });
+  });
+
   $('.back-to-top').click(function(){
     $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
     return false;
